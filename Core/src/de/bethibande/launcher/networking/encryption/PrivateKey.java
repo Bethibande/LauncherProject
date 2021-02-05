8adf30d4939bd0847a33dc68b4560b1c0e531769
bethibande <ph95Mouse>
2021-02-05 10:28:07 +0100

    save - 5th february 2021 Added parseString and toString method to PublicKey.java and PrivateKey.java

--- Core/src/de/bethibande/launcher/networking/encryption/PrivateKey.java
+++ Core/src/de/bethibande/launcher/networking/encryption/PrivateKey.java
@@ -16,4 +16,14 @@ public class PrivateKey {
         this.N = N;
     }
 
+    @Override
+    public String toString() {
+        return N.toString() + ";" + d.toString();
+    }
+
+    public static PrivateKey parseString(String s) {
+        String[] keys = s.split(";");
+        return new PrivateKey(new BigInteger(keys[1]), new BigInteger(keys[0]));
+    }
+
 }
--- Core/src/de/bethibande/launcher/networking/encryption/PublicKey.java
+++ Core/src/de/bethibande/launcher/networking/encryption/PublicKey.java
@@ -16,4 +16,14 @@ public class PublicKey {
         this.N = N;
     }
 
+    @Override
+    public String toString() {
+        return N.toString() + ";" + e.toString();
+    }
+
+    public static PublicKey parseString(String s) {
+        String[] keys = s.split(";");
+        return new PublicKey(new BigInteger(keys[1]), new BigInteger(keys[0]));
+    }
+
 }
